25b19566526948eba801c96a5802c00cc42a4ab9
tomfegan <tomfegan83@gmail.com>
2024-06-07 13:47:13 +0100

    Set up project and GH repo: used Spring Initializr to create the starter project for the Spring Boot application (with Maven dependency management and dependencies e.g., database driver, Spring Data JPA, Spring Web and Spring Actuator). Project files and folders that should not be committed to the GitHub moved to .gitignore file. created new GitHub repository for this project

---
+++ src/test/java/com/northcodes/recordshop/RecordshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcodes.recordshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
